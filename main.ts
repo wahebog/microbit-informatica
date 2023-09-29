0d0c72f73e54994f9208221e5d62d5c756c56b29
wahebog <145773283+wahebog@users.noreply.github.com>
2023-09-29 13:35:14 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+function Finitstate () {
+    FS = [
+    "010203040506",
+    "010708000920",
+    "070208",
+    81403151600,
+    0
+    ]
+}
+let FS: string[] = []
+let wachtij = 0
+let teller = ""
 basic.forever(function () {
 	
 })
